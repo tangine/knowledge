a10cf1ae9b78642ea6745fadddf47ee77187690d
汪承祥 <worklibo@foxmail.com>
2025-12-17 18:50:10 +0800

    更改系统名称

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'fulgens'
+project = '我的知识库'
 copyright = '2025, wangcx'
 author = 'wangcx'
 release = 'v1.0'
